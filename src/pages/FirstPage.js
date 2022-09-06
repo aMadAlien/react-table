8db4845572c4f78a1ecc8331d50c87299de493f3
iren <irasibil2@gmail.com>
2022-09-06 13:09:07 +0300

    included data and columns to pages

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,11 +1,99 @@
-import Data from "../components/Data";
 import Table from "../components/Table";
+import React, {Fragment, useMemo} from "react";
+import {compareItems} from "@tanstack/match-sorter-utils";
+import {sortingFns} from "@tanstack/react-table";
+import makeData from "../makeData";
+
+const fuzzySort = (rowA, rowB, columnId) => {
+    let dir = 0
+
+    // Only sort by ranking the column has ranking information
+    if (rowA.columnFiltersMeta[columnId]) {
+        dir = compareItems(
+            rowA.columnFiltersMeta[columnId]?.itemRank,
+            rowB.columnFiltersMeta[columnId]?.itemRank
+        )
+    }
+
+    // Provide an alphanumeric fallback for when the item ranks are equal
+    return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+}
+
+const Columns = ({PAGE_NUMBER}) => {
+    return useMemo(
+        () => [
+            {
+                header: `Name ${PAGE_NUMBER}`,
+                footer: props => props.column.id,
+                columns: [
+                    {
+                        accessorKey: 'firstName',
+                        cell: info => info.getValue(),
+                        footer: props => props.column.id,
+                    },
+                    {
+                        accessorFn: row => row.lastName,
+                        id: 'lastName',
+                        cell: info => info.getValue(),
+                        header: () => <span>Last Name</span>,
+                        footer: props => props.column.id,
+                    },
+                    {
+                        accessorFn: row => `${row.firstName} ${row.lastName}`,
+                        id: 'fullName',
+                        header: 'Full Name',
+                        cell: info => info.getValue(),
+                        footer: props => props.column.id,
+                        filterFn: 'fuzzy',
+                        sortingFn: fuzzySort,
+                    },
+                ],
+            },
+            {
+                header: `Info ${PAGE_NUMBER}`,
+                footer: props => props.column.id,
+                columns: [
+                    {
+                        accessorKey: 'age',
+                        header: () => 'Age',
+                        footer: props => props.column.id,
+                    },
+                    {
+                        header: 'More Info',
+                        columns: [
+                            {
+                                accessorKey: 'visits',
+                                header: () => <span>Visits</span>,
+                                footer: props => props.column.id,
+                            },
+                            {
+                                // accessorKey: 'status',
+                                accessorFn: row => row.relationship_status,
+                                id: 'status',
+                                header: 'Status',
+                                footer: props => props.column.id,
+                            },
+                            {
+                                accessorKey: 'progress',
+                                header: 'Profile Progress',
+                                footer: props => props.column.id,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+        []
+    )
+}
 
 const FirstPage = () => {
     const PAGE_NUMBER = 1;
-    const columns = Table({PAGE_NUMBER});
+    const columns = Columns({PAGE_NUMBER});
     return (
-        <Data columns={columns} />
+        <Fragment>
+            <Table makeData={makeData} columns={columns} />
+        </Fragment>
     )
 }
 
--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,11 +1,97 @@
-import Data from "../components/Data";
 import Table from "../components/Table";
+import makeData from "../makeData";
+import React, {useMemo} from "react";
+import {compareItems} from "@tanstack/match-sorter-utils";
+import {sortingFns} from "@tanstack/react-table";
+
+const fuzzySort = (rowA, rowB, columnId) => {
+    let dir = 0
+
+    // Only sort by ranking the column has ranking information
+    if (rowA.columnFiltersMeta[columnId]) {
+        dir = compareItems(
+            rowA.columnFiltersMeta[columnId]?.itemRank,
+            rowB.columnFiltersMeta[columnId]?.itemRank
+        )
+    }
+
+    // Provide an alphanumeric fallback for when the item ranks are equal
+    return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+}
+
+const Columns = ({PAGE_NUMBER}) => {
+    return useMemo(
+        () => [
+            {
+                header: `Name ${PAGE_NUMBER}`,
+                footer: props => props.column.id,
+                columns: [
+                    {
+                        accessorKey: 'firstName',
+                        cell: info => info.getValue(),
+                        footer: props => props.column.id,
+                    },
+                    {
+                        accessorFn: row => row.lastName,
+                        id: 'lastName',
+                        cell: info => info.getValue(),
+                        header: () => <span>Last Name</span>,
+                        footer: props => props.column.id,
+                    },
+                    {
+                        accessorFn: row => `${row.firstName} ${row.lastName}`,
+                        id: 'fullName',
+                        header: 'Full Name',
+                        cell: info => info.getValue(),
+                        footer: props => props.column.id,
+                        filterFn: 'fuzzy',
+                        sortingFn: fuzzySort,
+                    },
+                ],
+            },
+            {
+                header: `Info ${PAGE_NUMBER}`,
+                footer: props => props.column.id,
+                columns: [
+                    {
+                        accessorKey: 'age',
+                        header: () => 'Age',
+                        footer: props => props.column.id,
+                    },
+                    {
+                        header: 'More Info',
+                        columns: [
+                            {
+                                accessorKey: 'visits',
+                                header: () => <span>Visits</span>,
+                                footer: props => props.column.id,
+                            },
+                            {
+                                // accessorKey: 'status',
+                                accessorFn: row => row.relationship_status,
+                                id: 'status',
+                                header: 'Status',
+                                footer: props => props.column.id,
+                            },
+                            {
+                                accessorKey: 'progress',
+                                header: 'Profile Progress',
+                                footer: props => props.column.id,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+        []
+    )
+}
 
 const SecondPage = () => {
     const PAGE_NUMBER = 2;
-    const columns = Table({PAGE_NUMBER});
+    const columns = Columns({PAGE_NUMBER});
     return (
-        <Data columns={columns}/>
+        <Table makeData={makeData} columns={columns} />
     )
 }
 
